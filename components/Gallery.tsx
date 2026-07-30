41df724c0cfea0a8c99264fea1f8caa66956bb63
jay02 <dlawodud30325@naver.com>
2026-07-30 10:34:48 +0900

    style: replace h3.jpg with 14.jpg in collage

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -161,7 +161,7 @@ export function Gallery() {
 
           {/* 플로팅 사진 4 (우측 하단) */}
           <div className="absolute bottom-6 -right-6 w-[26%] z-20 shadow-lg rounded-[1px]">
-            <Photo name="h3.jpg" aspect="aspect-[2/3]" objectPosition="center" />
+            <Photo name="14.jpg" aspect="aspect-[2/3]" objectPosition="center" />
           </div>
           {/* 플로팅 사진 5 (좌측 상단) */}
           <div className="absolute top-6 left-4 w-[24%] z-20 shadow-lg rounded-[1px]">
