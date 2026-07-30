f5042cec9ede331a480c3eafc4ed152cf979efbc
jay02 <dlawodud30325@naver.com>
2026-07-30 11:00:22 +0900

    feat: add h4.jpg photo below the 3-column layout

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -175,10 +175,15 @@ export function Gallery() {
         </FadeIn>
 
         {/* 7. 우측 정렬 3칸 배치 (h1, h2, h3) */}
-        <FadeIn className="ml-auto w-[75%] mb-32 grid grid-cols-3 gap-2 pr-4">
+        <FadeIn className="ml-auto w-[75%] mb-24 grid grid-cols-3 gap-2 pr-4">
           <Photo name="h1.jpg" aspect="aspect-[2/3]" objectPosition="center" />
           <Photo name="h2.jpg" aspect="aspect-[2/3]" objectPosition="center" />
           <Photo name="h3.jpg" aspect="aspect-[2/3]" objectPosition="center" />
+        </FadeIn>
+
+        {/* 8. h4번 사진 단독 크게 배치 */}
+        <FadeIn className="w-[85%] mx-auto mb-32">
+          <Photo name="h4.jpg" aspect="aspect-[2/3]" objectPosition="center" />
         </FadeIn>
 
         {/* 하단 썸네일 뷰 (전체 사진 작은 사이즈) */}
