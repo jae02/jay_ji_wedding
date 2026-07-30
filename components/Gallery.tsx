b7de90f7be137ce8f8c38a6897b7a80d9f2bd8de
jay02 <dlawodud30325@naver.com>
2026-07-30 10:29:56 +0900

    style: swap h3.jpg and 16.jpg in the collage layout

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -161,12 +161,12 @@ export function Gallery() {
 
           {/* 플로팅 사진 4 (우측 하단) */}
           <div className="absolute bottom-6 -right-6 w-[20%] z-20 shadow-lg rounded-[1px]">
-            <Photo name="16.jpg" aspect="aspect-[2/3]" objectPosition="center" />
+            <Photo name="h3.jpg" aspect="aspect-[2/3]" objectPosition="center" />
           </div>
           
           {/* 플로팅 사진 5 (중앙 우측) */}
           <div className="absolute top-1/2 -translate-y-1/2 mt-8 right-12 w-[18%] z-20 shadow-lg rounded-[1px]">
-            <Photo name="h3.jpg" aspect="aspect-[2/3]" objectPosition="center" />
+            <Photo name="16.jpg" aspect="aspect-[2/3]" objectPosition="center" />
           </div>
         </FadeIn>
 
